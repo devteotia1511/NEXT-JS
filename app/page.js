48c7f89789076804927488adac95c66075abad74
Dev <devteotia1511@gmail.com>
2025-07-23 14:15:43 +0530

    second commit

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ const page = () => {
     // <div className='bg-red-700 p-5 text-7xl flex justify-center font-mono'>
     //   I am Homepage
     // </div>
-    <div className='bg-emerald-700 p-5 text-xl flex justify-center font-mono m-4'>
+    <div className='bg-emerald-700 p-5 text-2xl flex justify-center border-3 border-emerald-900 rounded-3xl text-white font-serif mt-10'>
       <h1>Problem solved by Next.js - </h1>
       <ul className='px-4'>
         <li>Full Stack Solution</li>
